ef39971eee4a8e2504d54b05c786d60d2ba02066
Sujith Nair <sujithaturservice@gmail.com>
2025-07-28 21:07:05 +0530

    Cleanup

--- Assets/Scripts/_Main/ScriptableObjects/LadderCoordPreset.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-namespace _Main.ScriptableObjects
-{
-    [CreateAssetMenu(fileName = "LadderCoordPreset", menuName = "Scriptable Objects/Ladder Coord Preset", order = 0)]
-    public class LadderCoordPreset : GenericCoordPreset { }
-}
--- Assets/Scripts/_Main/ScriptableObjects/SnakeCoordPreset.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-namespace _Main.ScriptableObjects
-{
-    [CreateAssetMenu(fileName = "SnakeCoordPreset", menuName = "Scriptable Objects/SnakeCoordPreset", order = 0)]
-    public class SnakeCoordPreset : GenericCoordPreset { }
-}
